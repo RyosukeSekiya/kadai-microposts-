eaee491d3e74d76e819ecb153a2b225ae047b873
Ryosuke.Sekiya <up.imagine143@gmail.com>
2020-08-04 14:44:10 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,17 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup' , to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+    
+    #memberとcollectionの違い
+    #検索結果を表示する場合は、ユーザを特定しては意味がないからcollectionを使用する
+    #collectionは:idが含まれないURLを生成する
+  end  
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
